bfdc95ba3e80729a727fe96266880259620bbbae
seboo1995 <sebair_selmani@yahoo.com>
2019-09-14 00:02:39 +0200

    adding the IT directory and its contents

---
+++ exam/selmani_sebair/IT/basic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/usr/bin:/bin # I think I have problems with the .bashrc file with my OS so I need to export this path, but nothing will happen if you run the script
+mkdir made_by_my_script #making a new directory within sebair_selmani
+ls ../ >made_by_my_script/list_of_contents.txt # listing all the content and putting them in list_of_contents.txt
